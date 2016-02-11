2951da2254318d68272e6026e2c4348a00c98492
AsIf <iamthe.angrytoast@gmail.com>
2016-02-11 15:28:50 -0800

    add tests. change pkg to name

---
+++ test/integration/default/serverspec/localhost/default_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe 'ansible-python::default' do
+
+  describe package('python_build_dependencies') do
+    it { should be_installed.by('apt') }
+  end
+
+  describe package('python-dev') do
+    it { should be_installed.by('apt') }
+  end
+
+  describe package('python-dev') do
+    it { should be_installed.by('apt') }
+  end
+
+  describe package('python-dev') do
+    it { should be_installed.by('apt') }
+  end
+
+  describe package('python-setuptools') do
+    it { should be_installed.by('apt') }
+  end
+
+  describe package('python-pip') do
+    it { should be_installed.by('apt') }
+  end
+
+  describe package('python3') do
+    it { should be_installed.by('apt') }
+  end
+
+  describe package('python3-dev') do
+    it { should be_installed.by('apt') }
+  end
+
+  describe package('python3-setuptools') do
+    it { should be_installed.by('apt') }
+  end
+
+  describe package('python3-pip') do
+    it { should be_installed.by('apt') }
+  end
+
+end
